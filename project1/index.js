6be3575c2f8883c8717edf6874c65ff11c552856
ckvignesh <vigneshckprakash@gmail.com>
2022-02-28 11:58:28 +0530

    first done

--- project1/index.js
+++ project1/index.js
@@ -1,6 +1,1 @@
-import React from "react";
-import { ReactDOM } from "react-dom";
-
-const page = <h1>Testing </h1>;
-
-ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(<h1> Hello Everyone</h1>, document.getElementById("root"));
