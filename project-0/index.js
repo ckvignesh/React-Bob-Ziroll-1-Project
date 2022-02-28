bc48d4b6bc1e8d48bf7f7fd5f1879b8ba56aeb0d
ckvignesh <vigneshckprakash@gmail.com>
2022-02-28 16:24:57 +0530

    substituted with element variable

--- project-0/index.js
+++ project-0/index.js
@@ -1,5 +1,8 @@
 const h1 = document.createElement("h1");
 h1.textContent = "this is an imperative way to program";
 h1.className = "header";
+console.log(h1);
 
-document.getElementById("root").append(h1);
+const element = <h1 className="header">This is JSX header</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
