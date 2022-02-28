474906ae38bc9b14163cb6ffea0eff96f8acd5cf
ckvignesh <vigneshckprakash@gmail.com>
2022-02-28 12:00:40 +0530

    test

--- project1/index.js
+++ project1/index.js
@@ -1,1 +1,2 @@
-ReactDOM.render(<h1> Hello Everyone</h1>, document.getElementById("root"));
+ReactDOM.render(<h1>Hello There</h1>, document.getElementById("root"));
+ReactDOM.render(<h2> Hi there</h2>, document.getElementById("test"));
