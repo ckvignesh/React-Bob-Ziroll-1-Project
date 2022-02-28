c29911346c7fdc1aefd41e19511a9b881f843841
ckvignesh <vigneshckprakash@gmail.com>
2022-02-28 16:13:00 +0530

    navbar practice

--- project1/index.js
+++ project1/index.js
@@ -1,9 +1,75 @@
-ReactDOM.render(<h1>Hello There</h1>, document.getElementById("root"));
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        Navbar
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Home <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Features
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Pricing
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Dropdown link
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">
+                Action
+              </a>
+              <a class="dropdown-item" href="#">
+                Another action
+              </a>
+              <a class="dropdown-item" href="#">
+                Something else here
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+function MainContent() {
+  return <h1>Hello There</h1>;
+}
+
 ReactDOM.render(
-  <ul>
-    <li>Item one</li>
-    <li>Item two</li>
-    <li>Item three</li>
-  </ul>,
-  document.getElementById("test")
+  <div>
+    <Navbar />
+    <MainContent />
+  </div>,
+  document.getElementById("root")
 );
