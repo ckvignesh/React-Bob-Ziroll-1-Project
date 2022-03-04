72e014f0d66d3d9c7500cb5a37e4c5126e9f23db
ckvignesh <vigneshckprakash@gmail.com>
2022-03-04 20:21:09 +0530

    components creation

--- project-0/index.js
+++ project-0/index.js
@@ -1,14 +1,31 @@
-const page = (
-  <div>
-    <h1>hello h1</h1>
-    <h2>Hello h2</h2>
-    <ul>
-      <li>test</li>
-      <li>check</li>
-    </ul>
-  </div>
-);
+// const page = (
+//   <div>
+//     <img src="./react-logo.png" width="40px" />
+//     <h1>Fun facts about React</h1>
+//     <ul>
+//       <li>Was first released in 2013</li>
+//       <li>Was originally created by Jordan Walke</li>
+//       <li>Has well over 100K Stars on Github</li>
+//       <li>Is maintained by Facebook</li>
+//       <li>Powers thousands of enterprise apps including mobile apps</li>
+//     </ul>
+//   </div>
+// );
 
-// document.getElementById("root").append(JSON.stringify(page));
-// document.getElementById("root").append(page);
-ReactDOM.render(page, document.getElementById("root"));
+function testFunc() {
+  return (
+    <div>
+      <img src="./react-logo.png" width="40px" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K Stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(testFunc(), document.getElementById("root"));
