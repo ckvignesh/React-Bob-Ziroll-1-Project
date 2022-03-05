5e46c9c594bfb025dfcf8b22c152da07b05bbb30
ckvignesh <vigneshckprakash@gmail.com>
2022-03-05 22:34:51 +0530

    added project 1

---
+++ project-1-react-infopage-local-setup/src/components/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <nav>
+      <img src="" alt="React Logo" className="nav--icon" />
+    </nav>
+  );
+}
--- project-1-react-infopage-local-setup/src/index.js
+++ project-1-react-infopage-local-setup/src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
 
 ReactDOM.render(<h1>Hello There</h1>, document.getElementById("root"));
