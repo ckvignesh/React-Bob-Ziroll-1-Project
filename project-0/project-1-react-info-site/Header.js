8116e1558fa483c9e9dd0aad8d6a851cf2e5dd21
ckvignesh <vigneshckprakash@gmail.com>
2022-03-05 22:33:59 +0530

    merged CDN project with project 0

---
+++ project-0/project-1-react-info-site/Header.js
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img src="./react-logo.png" className="nav-logo" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export { Header };
