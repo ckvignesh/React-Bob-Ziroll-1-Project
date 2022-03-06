953a09b112124d9896a64bcea63953a149b71ed2
ckvignesh <vigneshckprakash@gmail.com>
2022-03-06 15:17:44 +0530

    changed bullet color and added react logo

--- project-1-react-infopage-local-setup/src/components/Main.js
+++ project-1-react-infopage-local-setup/src/components/Main.js
@@ -2,15 +2,15 @@ import React from "react";
 
 export default function Main() {
   return (
-    <div className="main--main">
-      <h1 className="main--heading">Fun Facts about React</h1>
-      <ul className="main--list">
+    <main>
+      <h1 className="main--title">Fun Facts about React</h1>
+      <ul className="main--facts">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars on GitHub</li>
         <li>Is maintainerd by Facebook</li>
         <li>Powers thousands of Enterprise Apps, including mobile Apps</li>
       </ul>
-    </div>
+    </main>
   );
 }
