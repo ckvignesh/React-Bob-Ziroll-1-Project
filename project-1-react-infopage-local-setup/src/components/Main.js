67a6b2eeff8878cfbb71d4427f0963abe681b7e2
ckvignesh <vigneshckprakash@gmail.com>
2022-03-06 11:24:24 +0530

    prepared main section

--- project-1-react-infopage-local-setup/src/components/Main.js
+++ project-1-react-infopage-local-setup/src/components/Main.js
@@ -2,8 +2,15 @@ import React from "react";
 
 export default function Main() {
   return (
-    <div>
-      <h1>Main component</h1>
+    <div className="main--main">
+      <h1 className="main--heading">Fun Facts about React</h1>
+      <ul className="main--list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintainerd by Facebook</li>
+        <li>Powers thousands of Enterprise Apps, including mobile Apps</li>
+      </ul>
     </div>
   );
 }
