944172f97b85f0aeeb935dcc43c3e5ba0fac4116
ckvignesh <vigneshckprakash@gmail.com>
2022-03-06 16:32:47 +0530

    corrected typo

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ export default function Main() {
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintainerd by Facebook</li>
+        <li>Is maintained by Facebook</li>
         <li>Powers thousands of Enterprise Apps, including mobile Apps</li>
       </ul>
     </main>
